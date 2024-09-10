fd9001d13ddc57b01aae1b580e1825a891a0d798
Tarcísio <tarcisiomenezes2019@hotmail.com>
2024-09-11 01:25:43 +0200

    Almost finishing Navbar

--- frontend/fluxcommerce/src/app/layout.tsx
+++ frontend/fluxcommerce/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Image from 'next/image';
 import "./globals.css";
 
 const geistSans = localFont({
@@ -25,10 +26,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header>
+          <div className="top">
+            <Image src="/Logo1.png" alt="" width={300} height={300} />
+            <form>
+              <input type="text" placeholder="Search for products, brands and much more..." />
+              <button>
+                <Image src="/lupa.png" alt="" width={30} height={30}  />
+              </button>
+            </form>
+            <div className="Auth">Authentication</div>
+          </div>
+          <div className="down">
+            <div className="LeftLinks">
+              <a href="#">Categories</a>
+              <a href="#">Daily Offers</a>
+              <a href="#">Download App</a>
+              <a href="#">Contact</a>
+              <a href="#">Sell</a>
+            </div>
+            <div className="RightLinks">
+              <a href="#">Create Account</a>
+              <a href="#">Login</a>
+              <a href="#"><Image src="/cart.png" alt="" height={30} width={30} /></a>
+            </div>
+          </div>
+        </header>
         {children}
+        <footer>Footer</footer>
       </body>
     </html>
   );
